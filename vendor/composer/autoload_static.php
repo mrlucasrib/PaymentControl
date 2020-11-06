0dd6e54b19273a8d71b0ce407704187f41e31ecd
Lucas Ribeiro <lmr2199@gmail.com>
2020-11-06 15:42:49 -0300

    rollback

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -66,16 +66,11 @@ class ComposerStaticInit83385c0cae116807a55816f185d8a111
         ),
     );
 
-    public static $classMap = array (
-        'Composer\\InstalledVersions' => __DIR__ . '/..' . '/composer/InstalledVersions.php',
-    );
-
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
             $loader->prefixLengthsPsr4 = ComposerStaticInit83385c0cae116807a55816f185d8a111::$prefixLengthsPsr4;
             $loader->prefixDirsPsr4 = ComposerStaticInit83385c0cae116807a55816f185d8a111::$prefixDirsPsr4;
-            $loader->classMap = ComposerStaticInit83385c0cae116807a55816f185d8a111::$classMap;
 
         }, null, ClassLoader::class);
     }
