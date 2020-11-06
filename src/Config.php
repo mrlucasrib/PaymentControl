ddaa141d5f490c950198b5864c2d47b763c51f23
Lucas Ribeiro <lmr2199@gmail.com>
2020-11-06 17:20:44 -0300

    ADD heroku db

--- src/Config.php
+++ src/Config.php
@@ -1,12 +1,14 @@
 <?php
+$db = parse_url(getenv("DATABASE_URL"));
+
 define('URL_BASE', '');
 define("DATA_LAYER_CONFIG", [
-    "driver" => "mysql",
-    "host" => "db",
-    "port" => "3306",
+    "driver" => "pgsql",
+    "host" => $db['host'],
+    "port" => $db['port'],
     "dbname" => "payments",
-    "username" => "admin",
-    "passwd" => "admin",
+    "username" => $db['user'],
+    "passwd" => $db['pass'],
     "options" => [
         PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
