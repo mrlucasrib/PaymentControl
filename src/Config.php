16880bea373326167d8d83125eb281051e4cc23a
Lucas Ribeiro <lmr2199@gmail.com>
2020-11-06 21:53:09 -0300

    Configured

--- src/Config.php
+++ src/Config.php
@@ -1,17 +1,15 @@
 <?php
 $db = parse_url(getenv("DATABASE_URL"));
-echo "\n\n\n\n\n\n\nData\n";
-var_dump($db);
+
 define('URL_BASE', '');
 define("DATA_LAYER_CONFIG", [
     "driver" => "pgsql",
     "host" => $db['host'],
     "port" => $db['port'],
-    "dbname" => ltrim($db["path"], "/"),
+    "dbname" => ltrim($db['path'], "/"),
     "username" => $db['user'],
     "passwd" => $db['pass'],
     "options" => [
-        PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
         PDO::ATTR_CASE => PDO::CASE_NATURAL
